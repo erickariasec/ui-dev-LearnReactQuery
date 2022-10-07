471954b7ccecf500e4280bbe78c6b88341f7e1d3
erickariasec <erickarias1999@hotmail.com>
2022-10-07 00:14:26 -0500

    ui.dev React Query, Project: Setting up React Query

--- issue-tracker/src/main.jsx
+++ issue-tracker/src/main.jsx
@@ -4,6 +4,10 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
 import { worker } from "@uidotdev/react-query-api"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 new Promise((res) => setTimeout(res, 100))
 .then(() =>
@@ -15,11 +19,14 @@ new Promise((res) => setTimeout(res, 100))
 .then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <BrowserRouter>
-        <div className="container">
-          <App />
-        </div>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient} >
+        <BrowserRouter>
+          <div className="container">
+            <App />
+          </div>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.StrictMode>
   );
 });
