a03db5e8d76c4bc8ddb368fa1f892c3dd3543c5f
erickariasec <erickarias1999@hotmail.com>
2022-11-15 15:34:50 -0500

    ui.dev React Query, Project: DevTools & Fresh Queries (staleTime & Fresh)

--- issue-tracker/src/components/IssuesList.jsx
+++ issue-tracker/src/components/IssuesList.jsx
@@ -7,6 +7,8 @@ export default function IssuesList({labels, status}) {
     const statusString = status ? `&status=${status}` : ""
     const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
     return fetch(`/api/issues?${labelsString}${statusString}`).then((res) => res.json());
+  }, {
+    staleTime: 1000 * 60
   });
 
   const [searchValue, setSearchValue] = useState("");
