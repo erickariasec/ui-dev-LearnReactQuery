6565b12c3d71b24b0027d9bf1b7d2d588c258650
erickariasec <erickarias1999@hotmail.com>
2022-12-06 09:11:05 -0500

    ui.dev React Query, Issue Tracker, Project: Fetching States

--- issue-tracker/src/components/IssuesList.jsx
+++ issue-tracker/src/components/IssuesList.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import fetchWithError from "../helpers/fetchWithError";
 import { IssueItem } from "./IssueItem";
+import Loader from "./Loader";
 
 export default function IssuesList({ labels, status }) {
   const issuesQuery = useQuery(["issues", { labels, status }], ({ signal }) => {
@@ -46,7 +47,7 @@ export default function IssuesList({ labels, status }) {
           }}
         />
       </form>
-      <h2>Issues List</h2>
+      <h2>Issues List {issuesQuery.fetchStatus === "fetching" ? <Loader /> : null}</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
       ) : issuesQuery.isError ? (
